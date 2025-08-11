78556e244ae833d4910bab8ffbc4ffd576179e94
Veronica Soto <veronica.soto.s@ug.uchile.cl>
2025-08-11 16:25:01 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var d;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+ background(95,205,205);
+}
+
+function draw() {
+d = random(10,60);
+  fill(255,random(55,200));
+  ellipse(mouseX,mouseY,d,d);
+}
