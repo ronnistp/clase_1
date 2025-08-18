fe2e0112e2e5a0f81a0a41fc88bb14dd6fed015a
Veronica Soto <veronica.soto.s@ug.uchile.cl>
2025-08-18 17:50:53 -0400

    Add files via upload

---
+++ clase_1-main/sketch.js
@@ -0,0 +1,19 @@
+var d;
+
+function setup() {
+ createCanvas(windowWidth, windowHeight).position(0,0).style('z-index','-1');
+ background(95,205,205);
+}
+
+function draw() {
+d = random(10,60);
+   colorMode(HSB, 360, 100,100);
+  fill(random(0,360),100,100);
+  ellipse(mouseX,mouseY,d,d);
+}
+
+function keyPressed() {
+  if (key === 'p') {
+    save('dibujo.jpg');
+  }
+}
